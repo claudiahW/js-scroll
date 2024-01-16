c8971514654e004f7a16320ed0094051f251a273
claudiahW <murungiclaud@gmail.com>
2024-01-16 14:57:36 +0300

    add function to calculate heights of linksin navbar

--- scripts.js
+++ scripts.js
@@ -49,7 +49,18 @@ scrollLinks.forEach(function (link)) {
    e.preventDefault();
    // navigate to specific spot 
    const id = e.currentTarget.getAttribute("href").slice(1);
+   const element = document.getElementById(id);
+   // calculate heights
+   const navHeight = navbar.getBoundingClientRect().height;
+   const containerHeight = linksContainer.getBoundingClientRect().height;
+   const fixedNav = navbar.classList.contains("fixed-nav");
+   let position = element.offsetTop - navHeight;
 
+   window.scrollTo({
+      left:0,
+      top: position,
+   });
+   linksContainer.style.height = 0;
    });
 
 };
