9ca4d7b39cd24fdc3f2ec07ee2e24913f1f4b468
claudiahW <murungiclaud@gmail.com>
2024-01-16 15:07:35 +0300

    finish up

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+// select span
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
@@ -12,55 +13,90 @@ const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
-// add eventlistener to listen for a click
-navToggle.addEventListener("click", function(){
-   const containerHeight = linksContainer.getBoundingClientRect ();
-   const linksHeight = links.getBoundingClientRect().height;
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
 
-   if(containerHeight === 0 ){
-      linksContainer.style.height = `${linksHeight}px`;
-   }
-   else{
-      linksContainer.style.height = 0;
-   }
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+  // console.log(linksContainer.getBoundingClientRect());
 });
+
+// ********** fixed navbar ************
+
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
-// ********** fixed navbar ************
-window.addEventListener("scroll", function(){
-   const scrollHeight = window.pageYOffset;
-   const navHeight = navbar.getBoundingClientRect().height;
-   if(scrollHeight > navHeight){
-      navbar.classList.add("fixed-nav");
-   }
-   else{
-      navbar.classList.remove("fixed-nav");
-   }
-}); 
 
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+    console.log("helo");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelector(".scroll-link");
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
 
-scrollLinks.forEach(function (link)) {
-   link.addEventListener("click", function(e) {
-      // prevent default
-   e.preventDefault();
-   // navigate to specific spot 
-   const id = e.currentTarget.getAttribute("href").slice(1);
-   const element = document.getElementById(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    linksContainer.style.height = 0;
+  });
+});
    // calculate heights
    const navHeight = navbar.getBoundingClientRect().height;
    const containerHeight = linksContainer.getBoundingClientRect().height;
    const fixedNav = navbar.classList.contains("fixed-nav");
    let position = element.offsetTop - navHeight;
 
+   if(!fixednav){
+      position = position - navHeight;
+   }
+   if(navHeight > 82){
+      position = position + containerHeight;
+   }
    window.scrollTo({
       left:0,
       top: position,
    });
    linksContainer.style.height = 0;
-   });
+   
 
-};
